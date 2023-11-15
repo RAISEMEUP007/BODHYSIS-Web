ef2bdb7ed664142bc8dd498add17cfbd7a5a49d7
AnttoLion <anttolion@gmail.com>
2023-11-14 21:55:42 -0500

    styled for web

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { AuthScreen } from './screens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AuthScreen />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
