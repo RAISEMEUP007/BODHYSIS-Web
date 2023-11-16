8adbf8d358a024e58e5fcd7ac627b61a2fe2609a
AnttoLion <anttolion@gmail.com>
2023-11-16 09:45:54 -0500

    .env crashed when yarn web

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
+    ]
   };
 };
