8a7d4d4e76e52d25ecbbdcb9509e69ed10cc2ac2
sshere <sshere@exqsd.com>
2023-11-18 10:05:43 -0700

    changing url to env.js

--- constants/appConstants.js
+++ constants/appConstants.js
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native';
-//import {BOHDISYS_API_URL} from '../env';
-
-export const API_URL = Platform.OS == 'android' ? 'http://10.0.2.2:5000' : 'http://localhost:5000' ;
+import {BOHDISYS_API_URL} from '../env';
+export const API_URL = BOHDISYS_API_URL;
 
 export const COLORS = {
 };
