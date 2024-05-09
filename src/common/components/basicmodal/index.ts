48a8afe97f9979b6e6685f039ff1b8e1af8ac293
AnttoLion <anttolion@gmail.com>
2024-05-09 12:26:46 -0400

    Extras with brands

---
+++ src/common/components/basicmodal/index.ts
@@ -0,0 +1,5 @@
+export { commonModalStyle } from "./styles/commonModalStyle";
+export { default as BasicModalContainer } from "./BasicModalContainer";
+export { default as ModalBody } from "./ModalBody";
+export { default as ModalFooter } from "./ModalFooter";
+export { default as ModalHeader } from "./ModalHeader";
