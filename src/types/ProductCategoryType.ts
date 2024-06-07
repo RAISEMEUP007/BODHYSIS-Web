fa14b703f221b8f9388facb17771dd819242ddf2
AnttoLion <anttolion@gmail.com>
2024-06-06 20:05:56 -0400

    Some changes

--- src/types/ProductCategoryType.ts
+++
@@ -1,9 +0,0 @@
-export type ProductCategoryType = {
-  id: number;
-  category: string;
-  img_url: string | null;
-  description: 'dd';
-  tag_id: 1;
-};
-
-export type ProductCategoriesArrayType = Array<ProductCategoryType>;
